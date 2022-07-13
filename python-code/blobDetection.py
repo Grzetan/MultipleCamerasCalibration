23df55223ae955d469536de74b58525afcc21b26
Grzetan <grzetan@gmail.com>
2022-07-13 19:48:18 +0200

    Started implementing grid detection from detected blobs

--- python-code/blobDetection.py
+++ python-code/blobDetection.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
+import math
 
-im = cv2.imread('./sliced-imgs/center.jpg')
+im = cv2.imread('./sliced-imgs/rotatedRight.jpg')
 
 params = cv2.SimpleBlobDetector_Params()
 
@@ -18,6 +19,28 @@ else :
 detector.empty()
 keypoints = detector.detect(im)
 
-im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# Find index of top left blob on calibration plate
+idx = 0
+min_ = 1000000
+
+for i, keypoint in enumerate(keypoints):
+    x,y = keypoint.pt
+    if x+y < min_:
+        min_ = x+y
+        idx = i
+
+# Find index of blob to the right of top left blob
+min_dist = 10000000
+x, y = keypoints[idx].pt
+idx2 = 0
+for i, keypoint in enumerate(keypoints):
+    if i == idx: continue
+    x1, y1 = keypoints[i].pt
+    dist = math.sqrt((x1 - x)**2 + (y1 - y)**2)
+    if dist < min_dist:
+        min_dist = dist
+        idx2 = i
+
+im_with_keypoints = cv2.drawKeypoints(im, [keypoints[idx2]], np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 cv2.imshow("Keypoints", im_with_keypoints)
 cv2.waitKey(0)
